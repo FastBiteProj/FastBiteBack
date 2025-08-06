11ef14bdd3017200affc1829dd12804a779183a5
mrknsol <mrkonsol@mail.ru>
2025-08-06 22:09:31 +0400

    last commit before host

--- FastBite/FastBite.Presentation/Program.cs
+++ FastBite/FastBite.Presentation/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:5173", 
                 "http://localhost:5174",
-                "https://fast-bite-front.vercel.app/"
+                "https://fast-bite-front.vercel.app"
             )
             .AllowAnyHeader() 
             .AllowAnyMethod() 
